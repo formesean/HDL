43124c7b6385d849c85f592ba2adad6d7d0643c6
formesean <seanaguilar698@gmail.com>
2024-12-13 01:26:38 +0800

    fix: clock divider and pin assignments

--- JK_Flip_Flop/JK_FF.v
+++ JK_Flip_Flop/JK_FF.v
@@ -5,6 +5,34 @@
 *	Group/Schedule:	Group 3 - Friday, 7:30 AM to 10:30 AM
 *	Description:		JK_FF.v Module
 *********************************************************/
+module clock_divider(
+    input wire clk_50MHz,    
+    input wire reset,       
+    output reg clk_1Hz      
+);
+
+reg [25:0] counter; 
+
+always @(posedge clk_50MHz or posedge reset) 
+begin
+    if (reset) 
+    begin
+        counter <= 0;
+        clk_1Hz <= 0;
+    end else 
+    begin
+        counter <= counter + 1;
+        
+        if (counter == 25_000_000) 
+        begin
+            clk_1Hz <= ~clk_1Hz; 
+            counter <= 0;         
+        end
+    end
+end
+
+endmodule
+
 module JK_FF(
 	input wire J, K, nReset, Clk,
 	output reg Q, Qbar   
@@ -43,5 +71,29 @@ begin
 		endcase
 	end
 end
+
+endmodule
+
+module top(
+    input wire clk_50MHz,   
+    input wire J, K, nReset,
+	 output wire Q, Qbar
+);
+
+    wire clk_1Hz;  
+
+    clock_divider clk_div(
+        .clk_50MHz(clk_50MHz),
+        .clk_1Hz(clk_1Hz)
+    );
+
+    JK_FF jk_ff(
+			.J(J),
+			.K(K),
+			.nReset(nReset),
+			.Clk(clk_1Hz),
+			.Q(Q),
+			.Qbar(Qbar)
+    );
 
 endmodule
